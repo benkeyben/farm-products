0788a293c592aea7e3af45d87cf952744dd8c39f
Benjamin Yankey <ominidroneprime@outlook.com>
2019-07-06 10:37:11 +0100

    Update index.js, Add modules files, Rename template files

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-
+const fs               =  require('fs'),
+      http             =  require('http'),
+      url              =  require('url'),
+      replaceTemplate  =  require('./modules/replaceTemplate');
+ 
 // CREATING SERVER
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === '/' || pathName === '/overview'){
-    res.end('This is the OVERVIEW');
-  }else if(pathName === '/product'){
-    res.end('This is the PRODUCT')
-  }else if(pathName === '/api'){
+
+  const {query, pathname} = url.parse(req.url, true);
+  // OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview'){
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
+    res.end(output);
+
+  // PRODUCT PAGE
+  }else if(pathname === '/product'){
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+  //API
+  }else if(pathname === '/api'){
     res.writeHead(200, {
       'Content-type': 'application/json'
     });
     res.end(data);
+
+  // NOT FOUND PAGE
   }else{
     res.writeHead(404, {
       'Content-type': 'text/html',
